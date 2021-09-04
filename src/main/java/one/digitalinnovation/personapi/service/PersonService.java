6a8d68b2ac2967ecaf2e47761a9f49b8f17b92b3
felipepellizzon <fapellizzon@gmail.com>
2021-09-04 15:19:42 -0300

    colocado metodo update - fim parte 10

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -28,10 +27,7 @@ public class PersonService {
     public MessageResponseDTO createPerson(PersonDTO personDTO){
         Person personToSave = personMapper.toModel(personDTO);
         Person savedPerson = personRepository.save(personToSave);
-        return MessageResponseDTO
-                .builder()
-                .message("Created Person with ID " + savedPerson.getId())
-                .build();
+        return createMessageResponse(savedPerson.getId(), "Created Person with ID ");
     }
 
     public List<PersonDTO> listAll() {
@@ -52,9 +48,23 @@ public class PersonService {
         personRepository.deleteById(id);
     }
 
-    private Person verifyIfExists(Long id) throws PersonNotFoundException {
-       return personRepository.findById(id).orElseThrow(()-> new PersonNotFoundException(id));
+
+    public MessageResponseDTO updateById(Long id, PersonDTO personDTO) throws PersonNotFoundException {
+        verifyIfExists(id);
+        Person personToUpdate = personMapper.toModel(personDTO);
+
+        Person uptadedPerson = personRepository.save(personToUpdate);
+        return createMessageResponse(uptadedPerson.getId(), "Updated Person with ID ");
     }
 
+    private Person verifyIfExists(Long id) throws PersonNotFoundException {
+        return personRepository.findById(id).orElseThrow(()-> new PersonNotFoundException(id));
+    }
 
+    private MessageResponseDTO createMessageResponse(Long id, String message) {
+        return MessageResponseDTO
+                .builder()
+                .message(message + id)
+                .build();
+    }
 }
