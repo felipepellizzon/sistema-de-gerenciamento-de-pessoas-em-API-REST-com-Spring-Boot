56c00bf5eff29c1592c615db1466084d8b489b6e
felipepellizzon <fapellizzon@gmail.com>
2021-09-05 15:51:14 -0300

    Criacao arquivo README

---
+++ README.md
@@ -0,0 +1,17 @@
+<h2>Projeto de Desenvolvimento de um Sistema Gerenciador de Pessoas</h2>
+
+Neste projeto, foi desenvolvido um pequeno sistema para gerenciamento de pessoas em API-REST e com Spring Boot e com
+deploy para nuvem na plataforma Heroku.
+
+O projeto foi desenvolvido como exercício para para o Bootcamp do Santander na Digital Innovation One.
+
+Para execução do projeto é necessário ter os seguintes requisitos:
+* Java 11 ou superior.
+* Maven 3.8.1 ou superior.
+* IntelliJ IDEA Community Edition ou outra IDE.
+* Controle de versão GIT.
+* Conta no GitHub para armazenamento do projeto na nuvem.
+
+Segue os links para o repositório do GitHub e do Heroku.
+
+[Repositório GitHub](https://github.com/felipepellizzon/sistema-de-gerenciamento-de-pessoas-em-API-REST-com-Spring-Boot)
--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,28 +1,26 @@
 package one.digitalinnovation.personapi.controller;
 
+import lombok.AllArgsConstructor;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
-import one.digitalinnovation.personapi.entity.Person;
+
 import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.ReportAsSingleViolation;
+
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
     private PersonService personService;
 
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
 
 
     @PostMapping
